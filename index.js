156033ff0e3db5a79b7a0a50341fc1a155d8ef79
itsAbhirajSinha <mailabhiraj123@gmail.com>
2024-03-29 17:32:07 +0530

    initial commit backend

---
+++ index.js
@@ -0,0 +1,50 @@
+
+const dotenv = require('dotenv');
+const express = require('express')
+const { AccessToken } = require('livekit-server-sdk');
+const cors = require('cors')
+
+
+dotenv.config();
+
+const createToken = (roomName,participantName) => {
+
+  const at = new AccessToken(process.env.ENV_API_KEY, process.env.ENV_API_SECRET, {
+    identity: participantName,
+  });
+
+
+
+  at.addGrant({ roomJoin: true, room: roomName });
+
+  return at.toJwt();
+}
+
+const app = express();
+const port = 3000;
+
+
+const allowedOrigins = ['http://localhost:5173'];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
+
+
+app.get('/getToken', (req, res) => {
+ 
+  const { roomName, participantName} = req.query;
+  res.send(createToken(roomName,participantName));
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`)
+})
