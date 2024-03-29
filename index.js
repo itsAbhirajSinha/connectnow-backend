ddb36f7ccd3a872ff48a1e9f8d315b3adfb7e1b8
itsAbhirajSinha <mailabhiraj123@gmail.com>
2024-03-29 19:16:19 +0530

    testing json backend

--- index.js
+++ index.js
@@ -1,49 +1,53 @@
+// import dotenv from 'dotenv';
+// import express from 'express';
+// import { AccessToken } from 'livekit-server-sdk';
+// import cors from 'cors'
 const dotenv = require('dotenv');
-const express = require('express');
+const express = require('express')
 const { AccessToken } = require('livekit-server-sdk');
-const cors = require('cors');
+const cors = require('cors')
+
 
 dotenv.config();
 
-const createToken = (roomName, participantName) => {
+const createToken = (roomName,participantName) => {
+
   const at = new AccessToken(process.env.ENV_API_KEY, process.env.ENV_API_SECRET, {
     identity: participantName,
   });
-
   at.addGrant({ roomJoin: true, room: roomName });
 
   return at.toJwt();
-};
+}
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
-// Define allowed origins
 const allowedOrigins = ['https://connectnow-frontend.vercel.app'];
 
-// Configure CORS options
 const corsOptions = {
-  origin: function(origin, callback) {
-    if (allowedOrigins.includes(origin) || !origin) {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
     }
-  }
+  },
 };
 
-// Use CORS middleware with options
 app.use(cors(corsOptions));
 
-// Define route to generate token
+
 app.get('/getToken', (req, res) => {
-  const { roomName, participantName } = req.query;
-  res.send(createToken(roomName, participantName));
+ 
+  const { roomName, participantName} = req.query;
+  res.send(createToken(roomName,participantName));
 });
-app.get('/abhiraj',(req,res)=>{
-  return res.json({message:"testing "})
+
+app.get('/',(req,res)=>{
+  return res.json({message:"testing this"})
 })
-// Start the server
+
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+  console.log(`Server listening on port ${port}`)
+})
